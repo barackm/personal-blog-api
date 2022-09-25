539988dcb22df4c2ed0075b4242532436eac8a37
Barack Mukelenga <barackmukelenga100@gmail.com>
2022-09-25 22:01:08 +0200

    Create the project model

--- models/Project.js
+++ models/Project.js
@@ -1,3 +1,56 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
-const projectSchema = new mongoose.Schema({});
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  imageUrl: {
+    type: String,
+  },
+  sourceCodeUrl: {
+    type: String,
+  },
+  liveDemoUrl: {
+    type: String,
+  },
+  technologies: {
+    type: [String],
+    required: true,
+  },
+  tags: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+const validateProject = (project) => {
+  const schema = Joi.object({
+    title: Joi.string().min(5).max(255).required(),
+    description: Joi.string().min(5).max(1024).required(),
+    imageUrl: Joi.string(),
+    sourceCodeUrl: Joi.string(),
+    liveDemoUrl: Joi.string(),
+    technologies: Joi.array().items(Joi.string()).required(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  return schema.validate(project);
+};
+
+exports.Project = Project;
+exports.validate = validateProject;
--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { Project, validate } = require('../models/Project');
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  const projects = Project.find().sort({ createdAt: -1 });
+  res.send(projects);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const project = Project.findById(id);
+  if (!project)
+    return res.status(404).send('The project with the given ID was not found.');
+  res.send(project);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const project = new Project({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    sourceCodeUrl: req.body.sourceCodeUrl,
+    liveDemoUrl: req.body.liveDemoUrl,
+    technologies: req.body.technologies,
+    tags: req.body.tags,
+  });
+  try {
+    await project.save();
+    res.send(project);
+  } catch (ex) {
+    res.status(500).send('Something failed.');
+  }
 });
 
 module.exports = router;
