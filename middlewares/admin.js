00c304ff20d806675b7229f53b3b9e19e9e8d58d
Barack Mukelenga <barackmukelenga100@gmail.com>
2022-12-11 15:02:59 +0200

    Import the correct module for User model

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models/user');
+const { User } = require('../models/User');
 const {
   authorizationTokenString,
   JWT_PRIVATE_KEY,
